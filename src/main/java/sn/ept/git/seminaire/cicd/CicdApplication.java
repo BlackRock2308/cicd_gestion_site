cc027817ff07def625625e8ed564302da7a52889
aiMBF <83495094+aiMBF@users.noreply.github.com>
2022-04-24 03:10:53 +0200

    Update CicdApplication.java

--- src/main/java/sn/ept/git/seminaire/cicd/CicdApplication.java
+++ src/main/java/sn/ept/git/seminaire/cicd/CicdApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdApplication {
-
+public class CicdApplication extends SpringBootServletInitializer{
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(CicdApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
 	}
