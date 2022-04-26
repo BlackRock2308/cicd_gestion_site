919604b4f9a02e5c39acc452b8f51ddaf3d73da6
MBAYE SENE <smbaye@ept.sn>
2022-04-26 01:12:04 +0000

    feat: adding users-management classpath to catalina.properties

--- src/test/java/sn/ept/git/seminaire/cicd/mapper/MapperBaseTest.java
+++ src/test/java/sn/ept/git/seminaire/cicd/mapper/MapperBaseTest.java
@@ -1,14 +1,17 @@
 package sn.ept.git.seminaire.cicd.mapper;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import sn.ept.git.seminaire.cicd.CicdApplicationTests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = {CicdApplicationTests.class})
-@Transactional
+
+@SpringBootTest(classes = {CicdApplicationTests.class, DataSourceTransactionManagerAutoConfiguration.class})
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
 class MapperBaseTest {
 
     @Test
--- src/test/java/sn/ept/git/seminaire/cicd/repository/SocieteRepositoryTest.java
+++ src/test/java/sn/ept/git/seminaire/cicd/repository/SocieteRepositoryTest.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 class SocieteRepositoryTest extends RepositoryBaseTest {
 
     @Autowired
